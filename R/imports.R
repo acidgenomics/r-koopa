90c299e1a94510893ee1fb2d69dd91899d75fc83
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-11 14:53:04 -0400

    Initial commit

--- R/imports.R
+++ R/imports.R
@@ -1,2 +1,5 @@
-#' @importFrom acidbase realpath shell
+#' @importFrom acidbase realpath requireNamespaces sanitizeVersion shell
+#' @importFrom cli cli_div cli_end cli_ul
+#' @importFrom crayon green magenta red yellow
+#' @importFrom goalie isFlag isInstalled isString
 NULL
