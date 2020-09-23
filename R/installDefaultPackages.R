34387221c87083d30b6ddbf93f14bc0dc7b45f64
Michael Steinbaugh <mike@steinbaugh.com>
2020-09-23 09:53:59 -0400

    Install lobstr

--- R/installDefaultPackages.R
+++ R/installDefaultPackages.R
@@ -11,7 +11,7 @@
 #'   See https://github.com/tidyverse/tidyr/issues/1024 for details.
 #'
 #' @export
-#' @note Updated 2020-09-08.
+#' @note Updated 2020-09-23.
 #'
 #' @param all `logical(1)`.
 #'   Install additional extra packages.
@@ -87,6 +87,7 @@ installDefaultPackages <- function(all = FALSE) {
             "httr",
             "knitr",
             "lintr",
+            "lobstr",
             "magrittr",
             "matrixStats",
             "parallel",
