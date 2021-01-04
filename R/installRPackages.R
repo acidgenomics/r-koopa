6a15f538da8c6cc545467e448ae067605251460c
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-04 09:05:56 -0500

    Improve NAMESPACE

---
+++ R/installRPackages.R
@@ -0,0 +1,10 @@
+#' Install R packages
+#'
+#' @note Updated 2021-01-04.
+#' @export
+#'
+#' @examples
+#' ## > installRPackages()
+installRPackages <- function() {
+    installRecommendedPackages()
+}
