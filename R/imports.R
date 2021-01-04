48c65b3ee21bfc655c92b79d8944ee788234b4a4
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-04 09:12:08 -0500

    Migrate updateRPackages

--- R/imports.R
+++ R/imports.R
@@ -1,6 +1,6 @@
 #' @importFrom AcidBase basenameSansExt fileExt majorVersion minorVersion
 #'   realpath requireNamespaces sanitizeVersion shell
-#' @importFrom bb8 install installRecommendedPackages
+#' @importFrom bb8 install installRecommendedPackages updatePackages
 #' @importFrom cli cli_alert cli_alert_info cli_alert_success cli_div cli_end
 #'   cli_ul
 #' @importFrom crayon green magenta red yellow
---
+++ R/updateRPackages.R
@@ -0,0 +1,10 @@
+#' Update R packages
+#'
+#' @note Updated 2021-01-04.
+#' @export
+#'
+#' @examples
+#' ## > updateRPackages()
+updateRPackages <- function() {
+    updatePackages()
+}
