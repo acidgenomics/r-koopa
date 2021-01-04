57ab7f37bf5346ea9f08f7f5c6c6ff6f2916d05b
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-04 09:31:06 -0500

    Rework the NAMESPACE approach

--- R/imports.R
+++ R/imports.R
@@ -1,5 +1,5 @@
-#' @importFrom AcidBase basenameSansExt fileExt majorVersion minorVersion
-#'   realpath requireNamespaces sanitizeVersion shell
+#' @importFrom AcidBase basenameSansExt fileExt initDir majorVersion
+#'   minorVersion realpath requireNamespaces sanitizeVersion shell
 #' @importFrom AcidGenomes downloadEnsemblGenome downloadGencodeGenome
 #' @importFrom bb8 install installRecommendedPackages updatePackages
 #' @importFrom cli cli_alert cli_alert_info cli_alert_success cli_div cli_end
@@ -8,8 +8,8 @@
 #' @importFrom goalie allAreSystemCommands areDisjointSets assert hasGitHubPAT
 #'   hasLength hasNoDuplicates isADir isAFile isASymlink isCharacter isDocker
 #'   isFile isFlag isInstalled isLinux isMacOS isNumber isPackageVersion
-#'   isString isSubset isVanilla
+#'   isString isSubset
 #' @importFrom methods is
-#' @importFrom syntactic autopadZeros capitalize kebabCase
+#' @importFrom syntactic autopadZeros camelCase capitalize kebabCase snakeCase
 #' @importFrom utils install.packages tail
 NULL
