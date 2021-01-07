1612b2c252f12ebfdd3e446492fa7abb29de55a8
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-06 22:15:00 -0500

    Update NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -3,10 +3,10 @@
 #' @importFrom AcidCLI green h1 h2 h3 h4 h5 h6 h7 magenta red ul yellow
 #' @importFrom AcidGenomes downloadEnsemblGenome downloadGencodeGenome
 #' @importFrom bb8 install installRecommendedPackages updatePackages
-#' @importFrom goalie allAreSystemCommands areDisjointSets assert hasGitHubPAT
-#'   hasLength hasNoDuplicates isADir isAFile isASymlink isCharacter isDocker
-#'   isFile isFlag isInstalled isLinux isMacOS isNumber isPackageVersion
-#'   isString isSubset
+#' @importFrom goalie allAreDirs allAreSystemCommands areDisjointSets assert
+#'   hasGitHubPAT hasLength hasNoDuplicates isADir isAFile isASymlink
+#'   isCharacter isDocker isFile isFlag isInstalled isLinux isMacOS isNumber
+#'   isPackageVersion isString isSubset
 #' @importFrom methods is
 #' @importFrom syntactic autopadZeros camelCase capitalize kebabCase snakeCase
 #' @importFrom utils install.packages tail
