294d075d9d15f08b24b54c873676015d5c99ac02
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-15 08:45:38 -0500

    Ensure we run package checks

--- R/imports.R
+++ R/imports.R
@@ -2,7 +2,7 @@
 #'   minorVersion pasteURL realpath requireNamespaces sanitizeVersion shell
 #' @importFrom AcidCLI alert alertSuccess alertWarning green h1 h2 h3 h4 h5 h6
 #'   h7 magenta red ul yellow
-#' @importFrom bb8 install installRecommendedPackages updatePackages
+#' @importFrom bb8 check install installRecommendedPackages updatePackages
 #' @importFrom goalie allAreDirs allAreSystemCommands areDisjointSets assert
 #'   hasGitHubPAT hasLength hasNoDuplicates isADir isAFile isASymlink
 #'   isCharacter isDocker isFile isFlag isInstalled isLinux isMacOS isNumber
