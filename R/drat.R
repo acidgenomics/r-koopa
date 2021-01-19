1c4f8c392649d421232bec2efc2a052878ed4733
Michael Steinbaugh <mike@steinbaugh.com>
2021-01-19 12:46:47 -0500

    Update drat

--- R/drat.R
+++ R/drat.R
@@ -1,7 +1,7 @@
 #' Build a package and commit to drat repo
 #'
 #' @name drat
-#' @note Updated 2021-01-15.
+#' @note Updated 2021-01-19.
 #'
 #' @return `logical(1)`.
 #'
@@ -11,15 +11,11 @@ NULL
 
 
 
-## FIXME Ensure we sync bin and src with delete, size only.
-
-## FIXME Tag release and push automatically.
-## FIXME ERROR IF BRANCH ISNT ON MAIN / MASTER
-## FIXME NEED TO ADD CODE TO DETECT DEFAULT GIT BRANCH.
 .drat <- function(
     pkgDirs,
     repoDir = file.path("~", "monorepo", "drat")
 ) {
+    requireNamespaces("desc")
     assert(
         allAreDirs(pkgDirs),
         isADir(repoDir)
@@ -30,28 +26,32 @@ NULL
         X = pkgDirs,
         repoDir = repoDir,
         FUN = function(pkgDir, repoDir) {
-            ## FIXME CHECK THAT WERE ON DEFAULT BRANCH OF PACKAGE REPO,
-            ## OTHERWISE ERROR OUT.
-            ## FIXME NEED TO GET BRANCH
-            ## FIXME HOW TO CHECK GIT DEFAULT BRANCH AGAIN?
-            pkgName <- basename(pkgDir)
-            ## Handle `r-koopa` edge case.
-            if (any(grepl("-", pkgName))) {
-                pkgName <- strsplit(pkgName, "-")[[1L]][[2L]]
-            }
+            descFile <- file.path(pkgDir, "DESCRIPTION")
+            assert(isAFile(descFile))
             check(path = pkgDir)
+            .pkgdownDeployToAWS(pkg = pkgDir)
             tarball <- devtools::build(pkg = pkgDir)
             assert(isAFile(tarball))
-            ## FIXME THIS NEEDS TO SYNC BIN AND SRC WITH DELETE, SIZE-ONLY.
-            .pkgdownDeployToAWS(pkg = pkgDir)
             drat::insertPackage(
                 file = tarball,
                 repodir = repoDir,
                 action = "archive"
             )
             invisible(file.remove(tarball))
+            setwd(pkgDir)
+            name <- desc::desc_get_field(key = "Package", file = descFile)
+            version <- as.character(desc::desc_get_version(file = descFile))
+            today <- Sys.Date()
+            shell(
+                command = "git",
+                args = c(
+                    "tag", "-f",
+                    "-a", paste0("'v", version, "'"),
+                    "-m", paste0("'", name, " v", version, " (", today, ")'")
+                )
+            )
+            shell(command = "git", args = c("push", "--tags"))
             setwd(repoDir)
-            branch <- 
             shell(command = "git", args = c("checkout", "master"))
             shell(command = "git", args = c("fetch", "--all"))
             shell(command = "git", args = "merge")
@@ -79,15 +79,19 @@ NULL
 
 #' Deploy pkgdown website to AWS S3
 #'
-#' @note Updated 2021-01-15.
+#' @note Updated 2021-01-19.
 #' @noRd
 .pkgdownDeployToAWS <- function(
     pkg = ".",
     bucketDir = "s3://r.acidgenomics.com/packages"
 ) {
+    requireNamespaces(c("desc", "pkgdown"))
+    assert(isSystemCommand("aws"))
     pkgDir <- realpath(pkg)
-    pkgName <- basename(pkgDir)
-    bucketDir <- pasteURL(bucketDir, pkgName)
+    descFile <- file.path(pkgDir, "DESCRIPTION")
+    assert(isAFile(descFile))
+    pkgName <- desc::desc_get_field(key = "Package", file = descFile)
+    bucketDir <- pasteURL(bucketDir, tolower(pkgName))
     docsDir <- file.path(pkgDir, "docs")
     configFile <- file.path(pkgDir, "_pkgdown.yml")
     if (!isAFile(configFile)) {
@@ -104,14 +108,11 @@ NULL
         ),
         pkgName, docsDir, bucketDir
     ))
-    assert(isSystemCommand("aws"))
-    requireNamespaces("pkgdown")
     pkgdown::build_site(pkg = pkg)
     shell(
         command = "aws",
         args = c(
-            "s3",
-            "sync",
+            "s3", "sync", "--delete",
             paste0(docsDir, "/"),
             paste0(bucketDir, "/")
         )
