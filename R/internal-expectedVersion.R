3d0faacf6d3f1bdc127e6c34ad279a8db7380f58
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-11 09:29:07 -0500

    Add support for checking Python, Ruby, and Rust package versions

--- R/internal-expectedVersion.R
+++ R/internal-expectedVersion.R
@@ -1,6 +1,6 @@
 #' Expected version
 #'
-#' @note Updated 2021-01-06.
+#' @note Updated 2021-02-11.
 #' @noRd
 #'
 #' @param x `character`.
@@ -27,7 +27,11 @@
             assert(sum(keep, na.rm = TRUE) == 1L)
             x <- variables[keep]
             assert(isString(x))
-            x <- sub(pattern = "^(.+)=\"(.+)\"$", replacement = "\\2", x = x)
+            x <- sub(
+                pattern = "^(.+)=\"(.+)\"(.+)?$",
+                replacement = "\\2",
+                x = x
+            )
             x
         },
         FUN.VALUE = character(1L),
@@ -37,18 +41,21 @@
 
 
 
+## Updated 2021-02-11.
 .expectedHomebrewCaskVersion <- function(x) {
     .expectedVersion(paste0("homebrew-cask-", x))
 }
 
 
 
+## Updated 2021-02-11.
 .expectedMacOSAppVersion <- function(x) {
     .expectedVersion(x = paste0("macos-app-", tolower(x)))
 }
 
 
 
+## Updated 2021-02-11.
 .expectedMajorVersion <- function(x) {
     x <- .expectedVersion(x)
     x <- sanitizeVersion(x)
@@ -58,9 +65,31 @@
 
 
 
+## Updated 2021-02-11.
 .expectedMinorVersion <- function(x) {
     x <- .expectedVersion(x)
     x <- sanitizeVersion(x)
     x <- minorVersion(x)
     x
+}
+
+
+
+## Updated 2021-02-11.
+.expectedPythonPackageVersion <- function(x) {
+    .expectedVersion(x = paste("python", x, sep = "-"))
+}
+
+
+
+## Updated 2021-02-11.
+.expectedRubyPackageVersion <- function(x) {
+    .expectedVersion(x = paste("ruby", x, sep = "-"))
+}
+
+
+
+## Updated 2021-02-11.
+.expectedRustPackageVersion <- function(x) {
+    .expectedVersion(x = paste("rust", x, sep = "-"))
 }
