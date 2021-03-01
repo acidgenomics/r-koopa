b545355574a4e9867c9ce567158cab7f0d9f96c5
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-01 10:22:26 -0500

    Simplify the NAMESPACE

--- R/internal.R
+++ R/internal.R
@@ -1,3 +1,36 @@
+#' Detect HPC environment
+#'
+#' Detect if R is running on a high-performance computing (HPC) cluster.
+#'
+#' Currently supports detection of Slurm or LSF.
+#'
+#' @note Updated 2019-07-28.
+#' @noRd
+#'
+#' @return `character(1)` or `logical(1)`.
+#'   Workload manager (scheduler) name if HPC is detected (e.g. `"SLURM"` or
+#'   `"LSF"`), otherwise `FALSE`.
+#'
+#' @seealso
+#' - `Sys.getenv`.
+#' - `Sys.info`.
+#' - `R.version`.
+#' - `.Platform`.
+#'
+#' @examples
+#' .detectHPC()
+.detectHPC <- function() {
+    if (!identical(Sys.getenv("LSF_ENVDIR"), "")) {
+        "LSF"
+    } else if (!identical(Sys.getenv("SLURM_CONF"), "")) {
+        "SLURM"
+    } else {
+        FALSE
+    }
+}
+
+
+
 #' Check failure
 #'
 #' Set a system environment variable that we can detect in `koopa check`.
