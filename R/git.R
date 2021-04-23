995718b75027770dfd3ff48da12ea55c97381533
Michael Steinbaugh <mike@steinbaugh.com>
2021-04-23 11:20:02 -0400

    Initial commit of git remote branch utility

---
+++ R/git.R
@@ -0,0 +1,35 @@
+#' Default Git branch
+#'
+#' @note Updated 2021-04-23.
+#' @export
+#'
+#' @param repo `character(1)`.
+#'   Git repository directory path.
+#' @param remote `character(1)`.
+#'   Remote name.
+#'
+#' @seealso
+#' - `koopa::git_default_branch` (shell).
+#'
+#' @examples
+#' ## > repo <- file.path("~", "git", "monorepo")
+#' ## > gitDefaultBranch(repo)
+gitDefaultBranch <- function(repo, remote = "origin") {
+    assert(
+        isADir(repo),
+        isString(remote)
+    )
+    wd <- getwd()
+    setwd(repo)
+    x <- shell(
+        command = "git",
+        args = c("remote", "show", remote),
+        stdout = TRUE
+    )
+    pattern <- "^.+HEAD branch: ([^\\s]+)$"
+    x <- grep(pattern = pattern, x = x, value = TRUE)
+    x <- sub(pattern = pattern, replacement = "\\1", x = x)
+    assert(isString(x))
+    setwd(wd)
+    x
+}
